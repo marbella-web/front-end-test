7e0b291823c88b44a72ed5ed5fb4fb3edacfca5b
Stuart Hopkins <stuart.hopkins@netbuilder.com>
2019-10-04 17:01:42 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+	
+	$( ".terms h4").click(function() {
+  $(this).next().toggle( "slow", function() {
+    // Animation complete.
+  });
+});
+	
+
+
+
+	
+
+}); /* close document ready jquery */
+
+
+
