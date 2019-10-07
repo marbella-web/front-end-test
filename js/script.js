4f85fc51aab50be3ff6755199e066e944852349a
Stuart Hopkins <stuart.hopkins@netbuilder.com>
2019-10-08 00:58:40 +0200

    final files

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-$(document).ready(function(){
-	
-	
-	$( ".terms h4").click(function() {
-  $(this).next().toggle( "slow", function() {
-    // Animation complete.
+$(document).ready(function () {
+
+  $(".tabholder").show();
+  $(".tab").hide();
+  $(".tab:first").show();
+
+  $(".tabholder ul").children().each(function (i) {
+    $(this).click(function () {
+      var index = i;
+
+      $(".tabholder ul li, a").removeClass("active");
+
+      $(".tab-content li").hide();
+      $(".tab-content li").eq(i).show();
+
+      $(this).addClass("active");
+      $(this).find("a").addClass("active");
+
+
+    });
   });
-});
-	
 
 
 
-	
 
 }); /* close document ready jquery */
-
-
-
